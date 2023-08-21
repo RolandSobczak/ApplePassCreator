eef0d0400ce3a54f764375849affa5f68967a3f2
rolandsobczak <rolandsobczak@icloud.com>
2023-08-21 11:49:56 +0200

    feat: all files fixed by pre-commit hooks

--- src/apple_pass_creator/manifest_hash.py
+++ src/apple_pass_creator/manifest_hash.py
@@ -1,6 +1,6 @@
 from hashlib import sha1
-from pathlib import Path
 from json import dumps
+from pathlib import Path
 
 
 def calculate_sha1(callback) -> str:
@@ -20,9 +20,9 @@ def calculate_sha1(callback) -> str:
 
 
 def calculate_file_hash(
-        file_path: Path,
-        callback=calculate_sha1,
-        chunk_size: int = 2096
+    file_path: Path,
+    callback=calculate_sha1,
+    chunk_size: int = 2096,
 ) -> str:
     """This function returns hash of file from given path. By default, this function use sha1 algorithm
     to hash files. To change this behavior overwrite callback kwarg with function which takes callback.
