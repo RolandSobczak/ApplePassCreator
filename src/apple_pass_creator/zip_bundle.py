35530f1d1d037dbd0032bc5fd71fa783afe106f1
rolandsobczak <rolandsobczak@icloud.com>
2023-08-23 12:21:59 +0200

    fix: now paths and other_files in bundle_files function are optional

--- src/apple_pass_creator/zip_bundle.py
+++ src/apple_pass_creator/zip_bundle.py
@@ -1,6 +1,7 @@
 import io
 import zipfile
 from pathlib import Path
+from typing import Dict, List, Optional
 
 
 class Bundle:
@@ -17,7 +18,10 @@ class Bundle:
         return self._zip_bytes.read()
 
 
-def bundle_files(paths: list[Path], other_files: dict[str, bytes]) -> bytes:
+def bundle_files(
+    paths: Optional[List[Path]] = None,
+    other_files: Optional[Dict[str, bytes]] = None,
+) -> bytes:
     """This function helps with bundling files into zip files.
     Accept both paths to files and files as bytes.
 
@@ -29,10 +33,12 @@ def bundle_files(paths: list[Path], other_files: dict[str, bytes]) -> bytes:
     :rtype: bytes
     """
     bundle = Bundle()
-    for file_path in paths:
-        with open(file_path, mode="rb") as f:
-            bundle.add_file(file_path.name, f.read())
-    for filename, content in other_files.items():
-        bundle.add_file(filename, content)
+    if paths:
+        for file_path in paths:
+            with open(file_path, mode="rb") as f:
+                bundle.add_file(file_path.name, f.read())
+    if other_files:
+        for filename, content in other_files.items():
+            bundle.add_file(filename, content)
 
     return bundle.zip
